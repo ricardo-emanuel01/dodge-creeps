f7beead29e3812c1c04b6b87856e657f80d49df3
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-09-11 18:04:50 -0300

    :sparkles: The script attached to the Mob scene

---
+++ Mob.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Mob : RigidBody2D
+{	
+	private void OnVisibleOnScreenNotifier2DScreenExited() {
+		QueueFree();
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+		AnimatedSprite2D animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+		animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
