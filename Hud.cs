d7e6e8e75c74966503351849e46b698df7f93299
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-09-11 18:06:11 -0300

    :sparkles: Script attached to HUD scene

---
+++ Hud.cs
@@ -0,0 +1,62 @@
+using Godot;
+using System;
+
+public partial class Hud : CanvasLayer
+{
+	[Signal]
+	public delegate void StartGameEventHandler();
+	
+	public void ShowMessage(string text) {
+		var message = GetNode<Label>("Message");
+		message.Text = text;
+		message.Show();
+
+		GetNode<Timer>("MessageTimer").Start();
+	}
+	
+	async public void ShowGameOver() {
+		string gameOverMessage = "Game Over";
+		ShowMessage(gameOverMessage);
+
+		Timer messageTimer = GetNode<Timer>("MessageTimer");
+		await ToSignal(messageTimer, Timer.SignalName.Timeout);
+
+		Label message = GetNode<Label>("Message");
+		message.Text = "Dodge the\nCreeps!";
+		message.Show();
+		GetNode<Timer>("MessageTimer").Stop();
+
+		await ToSignal(GetTree().CreateTimer(1.0), SceneTreeTimer.SignalName.Timeout);
+		GetNode<Button>("StartButton").Show();
+	}
+	
+	public void UpdateScore(int score) {
+		GetNode<Label>("ScoreLabel").Text = score.ToString();
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	private void OnMessageTimerTimeout() {
+		GetNode<Label>("Message").Hide();
+	}
+	
+	private void OnStartButtonPressed() {
+		GetNode<Button>("StartButton").Hide();
+		GetNode<Label>("Message").Hide();
+		EmitSignal(SignalName.StartGame);
+	}
+}
+
+
+
+
+
+
